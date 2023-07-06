89287a37ecbbd9700b08f72440c231506670c5c6
RA4Z <rraz639@gmail.com>
2023-07-06 12:17:22 -0300

    Começando estilização do título

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,28 @@
 import Link from '../src/components/Link'
 
+function Title({ children, as }) {
+    const Tag = as;
+    return (
+        <>
+            <Tag>
+                {children}
+            </Tag>
+            <style jsx>
+                {`
+                ${Tag} {
+                    color: red;
+                    font-family: sans-serif;
+                }
+                `}
+            </style>
+        </>
+    )
+}
+
 export default function HomePage() {
     return (
         <div>
-            <h1>Alura Cases - Home Page</h1>
+            <Title as='h1'>Alura Cases - Home Page</Title>
             <Link href='/Faq'>
                 Ir para o FAQ
             </Link>
