d01007b682492cc43d8090b37c411706e4337791
RA4Z <rraz639@gmail.com>
2023-07-10 12:10:30 -0300

    Adicionando google analytics no projeto

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,20 @@ function MyApp({ Component, pageProps }) {
     return (
         <>
             <Head>
+                {/* <!-- Google tag (gtag.js) --> */}
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-43Q8FT2L3Q"></script>
+                <script
+                    dangerouslySetInnerHTML={{
+                        __html: `
+                            window.dataLayer = window.dataLayer || [];
+                            function gtag(){dataLayer.push(arguments);}
+                            gtag('js', new Date());
+
+                            gtag('config', 'G-43Q8FT2L3Q');
+                        `
+                    }}
+                />
+
                 <link rel="preconnect" href="https://fonts.googleapis.com" />
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
                 <link href="https://fonts.googleapis.com/css2?family=Italiana&family=Josefin+Sans:wght@400;700&family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap" rel="stylesheet" />
